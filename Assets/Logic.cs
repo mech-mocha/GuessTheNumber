279dc3ceae5e080e79d556cf9e53e8acb622cd6e
Shilp Gupta <shilp@mechmocha.com>
2016-07-24 12:16:03 +0530

    Add all the files

---
+++ Assets/Logic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LessClicked() {
+
+	}
+
+	public void MoreClicked() {
+
+	}
+}
